1ba0368290d4e5c1284eab7321603aaa95151a57
alain <alainlay@gmail.com>
2019-09-29 11:24:44 +1300

    Initialize all challenge attributes

--- src/main/resources/static/javascript/createChallenge.js
+++ src/main/resources/static/javascript/createChallenge.js
@@ -3,13 +3,14 @@ function postFormData() {
   postData({
     title: document.getElementById('title').value,
     description: document.getElementById('description').value,
-    subscriptionAmount: document.getElementById('amount').value,
-    subscriptionCap: document.getElementById('cap').value,
-    imageUrl: document.getElementById('image').value,
+    subscriptionAmount: document.getElementById('subscriptionAmount').value,
+    subscriptionCap: document.getElementById('subscriptionCap').value,
+    imageUrl: document.getElementById('imageUrl').value,
     challengePercentage: 0,
     challengeProgress: 0,
     challengeCap: 100,
-    challengeUnits: "Weeks"
+    challengeUnits: "Weeks",
+    penaltyAmount: 50,
   });
 
 }
