9d77cd197a1a72a8f0b8503f599e4ded4db1d395
alain <alainlay@gmail.com>
2019-09-28 18:58:02 +1200

    Add accessors to ChallengeFormSubmission

--- src/main/java/com/keepaccountable/challengeFormSubmission/ChallengeFormSubmission.java
+++ src/main/java/com/keepaccountable/challengeFormSubmission/ChallengeFormSubmission.java
@@ -1,16 +1,37 @@
 package com.keepaccountable.challengeFormSubmission;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.Data;
+import lombok.ToString;
 
 @Data
+@ToString
 public class ChallengeFormSubmission {
+	@JsonProperty("title")
 	private String title;
+	
+	@JsonProperty("description")
 	private String description;
+	
+	@JsonProperty("subscriptionAmount")
 	private Long subscriptionAmount;
+	
+	@JsonProperty("subscriptionCap")
 	private long subscriptionCap;
+	
+	@JsonProperty("imageUrl")
 	private String imageUrl;
+	
+	@JsonProperty("challengePercentage")
 	private Long challengePercentage;
+	
+	@JsonProperty("challengeProgress")
 	private Long challengeProgress;
+
+	@JsonProperty("challengeCap")
 	private Long challengeCap;
+	
+	@JsonProperty("challengeUnits")
 	private String challengeUnits;
 }
