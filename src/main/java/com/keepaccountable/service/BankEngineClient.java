2f708c0bad2d017e21f024522c4e981b8f5e50d8
Alan Gao <alan.gao@unimarket.com>
2019-09-28 21:36:44 +1200

    Working on BankEngine API client.

--- src/main/java/com/keepaccountable/service/BankEngineClient.java
+++ src/main/java/com/keepaccountable/service/BankEngineClient.java
@@ -1,43 +1,61 @@
 package com.keepaccountable.service;
 
 import com.keepaccountable.domain.Token;
+import lombok.extern.slf4j.Slf4j;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
 @Service
+@Slf4j
 public class BankEngineClient {
 
     @Autowired
     private RestTemplate restClient;
 
-
-    @Value("${bankengine.client.id}")
-    private String clientId;
-    @Value("${bankengine.client.secret}")
-    private String clientSecret;
+    private String authUrl = "https://auth.bankengine.nz/connect/token";
 
 
-    public Token exchangeToken() {
-//        HttpHeaders headers = new HttpHeaders();
-//        headers.setContentType(MediaType.TEXT_PLAIN);
-//
-//        MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
-//        map.add("grant_type", "authorization_code");
-//        map.add("client_id", clientId);
-//        map.add("client_secret", clientSecret);
-//        map.add("redirect_uri", "http://localhost:8080/callback");
-//
-//        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(map, headers);
-//
-//
-//        restClient.postForEntity("https://auth.bankengine.nz/connect/token", request, Token.class);
-        return null;
+    public Token exchangeToken(@NotNull String clientId, @NotNull String secret, @NotNull String code) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.TEXT_PLAIN);
+
+        MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
+        map.add("grant_type", "authorization_code");
+        map.add("client_id", clientId);
+        map.add("client_secret", secret);
+        map.add("redirect_uri", "http://localhost:8080/callback");
+        map.add("code", code);
+
+        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(map, headers);
+
+        ResponseEntity<Token> result = restClient.postForEntity(authUrl, request, Token.class);
+        log.info(result.getBody().toString());
+        return result.getBody();
+    }
+
+    public Token refreshToken(@NotNull String clientId, @NotNull String secret, @NotNull String refreshToken) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.TEXT_PLAIN);
+
+        MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
+        map.add("grant_type", "refresh_token");
+        map.add("client_id", clientId);
+        map.add("client_secret", secret);
+        map.add("redirect_uri", "http://localhost:8080/callback");
+        map.add("code", refreshToken);
+
+        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(map, headers);
+
+        ResponseEntity<Token> result = restClient.postForEntity(authUrl, request, Token.class);
+        log.info(result.getBody().toString());
+        return result.getBody();
     }
 }
