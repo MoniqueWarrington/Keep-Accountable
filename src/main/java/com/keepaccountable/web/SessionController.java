1a67cd04f951888bea6459cfe970700c21734ef1
alain <alainlay@gmail.com>
2019-09-28 20:54:06 +1200

    Add session expiry error handling

--- src/main/java/com/keepaccountable/web/SessionController.java
+++ src/main/java/com/keepaccountable/web/SessionController.java
@@ -1,6 +1,8 @@
 package com.keepaccountable.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.HttpSessionRequiredException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.SessionAttributes;
@@ -10,6 +12,12 @@ import org.springframework.web.servlet.ModelAndView;
 @SessionAttributes("token")
 public class SessionController {
 
+	@ExceptionHandler(HttpSessionRequiredException.class)
+	public ModelAndView redirect() {
+		ModelAndView modelAndView = new ModelAndView("sessionExpired");
+        return modelAndView;
+	}
+	
     @GetMapping("/session")
     public ModelAndView callback(@ModelAttribute("token") String token) {
     	ModelAndView modelAndView = new ModelAndView("session");
---
+++ src/main/resources/templates/sessionExpired.ftl
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Session expired</title>
+</head>
+
+<body>
+<p>Your session has expired please <a href="https://auth.bankengine.nz/banks?ReturnUrl=%2Fconnect%2Fauthorize%2Fcallback%3Fresponse_type%3Dcode%26client_id%3Dkeep-accountable%26redirect_uri%3Dhttp%253A%252F%252Flocalhost%253A8080%252Fcallback%26scope%3Duserinfo%2520offline_access%2520accounts%2520balance%2520transactions%2520payments%26nonce%3Dnonce%26state%3Dstate%2527">login</a> again</p>
+
+</body>
+</html>
