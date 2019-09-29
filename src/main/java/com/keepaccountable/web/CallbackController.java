6854df817ea460bf22a8997733bfa5e452184692
Alan Gao <alan.gao@unimarket.com>
2019-09-29 14:34:13 +1300

    refund issue.

--- src/main/java/com/keepaccountable/web/CallbackController.java
+++ src/main/java/com/keepaccountable/web/CallbackController.java
@@ -8,17 +8,16 @@ import lombok.extern.slf4j.Slf4j;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
 @Slf4j
 @Controller
-@SessionAttributes("token")
 public class CallbackController {
 
     @Autowired
@@ -30,17 +29,24 @@ public class CallbackController {
     }
 
     @GetMapping("/callback")
-    public ModelAndView callback(@RequestParam("code") String code, @ModelAttribute("token") String token) {
-        ModelAndView modelAndView = new ModelAndView("createChallenge");
-        Token t = client.exchangeToken(code);
-        modelAndView.addObject("token", t.getAccessToken());
-//        retriveAllInfo(code);
-        return modelAndView;
+    public ModelAndView callback(Model model, @RequestParam("code") String code) {
+        Token token = client.exchangeToken(code);
+        model.addAttribute(token);
+
+        UserInfo userInfo = client.getUserInfo(token.getAccessToken());
+        if ("alainlay@gmail.com".equals(userInfo.getEmails().get(0))) {
+            return new ModelAndView("callback");
+        } else {
+//            ModelAndView modelAndView = new ModelAndView("createChallenge");
+//            modelAndView.addObject("token", token.getAccessToken());
+            retriveAllInfo(code, token);
+            return new ModelAndView("callback");
+        }
     }
 
-    private void retriveAllInfo(@NotNull String code) {
-        Token token = client.exchangeToken(code);
-        UserInfo user = client.getUserInfo(token.getAccessToken());
+    private void retriveAllInfo(@NotNull String code, @NotNull Token token) {
+//        Token token = client.exchangeToken(code);
+//        UserInfo user = client.getUserInfo(token.getAccessToken());
         List<Account> accounts = client.getAllAccount(token.getAccessToken());
         accounts.forEach(account -> client.getBalance(account, token.getAccessToken()));
         accounts.forEach(account -> {
