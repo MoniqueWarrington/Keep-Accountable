282636590f21fe7c4ab3ae59b4b7d9ff8ed6a2a9
Ryan Brown <ryanb@bravurasolutions.com>
2019-10-10 15:53:42 +1300

    react styling refactoring

--- keep-accountable-react/src/ChallengeView/ChallengeView.js
+++ keep-accountable-react/src/ChallengeView/ChallengeView.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './../App.css';
+import './../App123.css';
 
 
 const styleJumbotron = {
---
+++ keep-accountable-react/src/Styles.js
@@ -0,0 +1,8 @@
+const styleJumbotron = {
+    padding: '20px', 
+    margin:'5px'
+}
+
+const styleText = {
+    'text-align': 'left'
+}
